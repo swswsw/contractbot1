95b49e1ad88ee1bd968652ec8816c6008b303897
sol wu <apiswswsw@gmail.com>
2024-10-19 11:13:21 -0700

    some changes reverted

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,33 @@
 import { run, HandlerContext } from "@xmtp/message-kit";
 
+// // save messages
+// let messages = [];
+// // save just the content.  for more clear logging
+// let messageContents:string[] = []; 
+
 run(async (context: HandlerContext) => {
   // Get the message and the address from the sender
   const { content, sender } = context.message;
+
+  //console.log('Sender:', sender, 'Content:', content);
+  console.log(content.content);
+
+  // // Save the message content in the array
+  // messages.push(context.message);
+  // messageContents.push(content.content);
+
+  // // Log the current state of the messages array (optional)
+  // console.log('Stored messages:', messageContents);
+
+  // if (content.content == "/new") {
+  //   // new conversation started.  clear the messages
+  //   messages = [];
+  //   messageContents = [];
+    
+  //   // To reply, just call `reply` on the HandlerContext.
+  //   await context.send(`New conversation started!!!  :)`);
+  // }
+
   // To reply, just call `reply` on the HandlerContext.
   await context.send(`gm`);
 });
